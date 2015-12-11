2440400e2711612146318f1c1fafef62ceaeba74
sq435 <sq435@hotmail.com>
2015-12-11 19:48:53 +0800

    chart

--- app/protected/controllers/ToiletController.php
+++ app/protected/controllers/ToiletController.php
@@ -10,12 +10,18 @@ class ToiletController extends Controller
 {
     public $bodyClass = 'landing';
     public $headerClass = 'alt';
-    
+
+    /**
+     * show intro page
+     */
     public function actionIntroduction()
     {
         $this->render('intro');
     }
 
+    /**
+     * show live page
+     */
     public function actionLive()
     {
         $this->bodyClass = '';
@@ -26,36 +32,74 @@ class ToiletController extends Controller
             'toilets' => $toilets
         ));
     }
-    
+
+    /**
+     * show chart page
+     */
     public function actionCharts()
     {
         $this->bodyClass = '';
         $this->headerClass = '';
-        
+        $returnData = $this->getChartData();
+        $this->render('charts', array(
+            'datas' => json_encode($returnData['datas']),
+            'timeJson' => json_encode($returnData['timeJson'])
+        ));
+    }
+
+    /**
+     * ajax get chart data
+     */
+    public function actionGetChart()
+    {
+        $date = $_POST['date'];
+        $returnData = $this->getChartData($date);
+        echo json_encode($returnData);
+    }
+
+    /**
+     * get chart data by date (default today)
+     * @param string|null $date
+     * @return array
+     */
+    private function getChartData($date=null)
+    {
+        if(empty($date))
+        {
+            $date = date('Y/m/d');
+        }
         $logs = Yii::app()->db->createCommand()
             ->select('HOUR(created_at) as hour, count(*) as count')
             ->from('toilet_event_logs')
-            ->where('spend_time > 1')
+            ->where("spend_time > 1 and to_days(created_at) = to_days('".$date."')")
             ->group('HOUR(created_at)')
             ->queryAll();
 
         $datas = array();
+        $timeArr = array();
         for($i=0; $i<=23; $i++)
         {
-
             $currentData = array_filter($logs, function($log) use ($i) {
                 if($log['hour'] == (string)$i)
                     return $log['count'];
             });
 
             $datas[$i] = empty($currentData) ? 0 : array_shift($currentData)['count'];
+            $timeArr[] = $i."時";
         }
-        
-        $this->render('charts', array(
-            'datas' => json_encode($datas)
-        ));
+
+        $returnData = array(
+            'datas' => $datas,
+            'timeJson' => $timeArr
+        );
+
+        return $returnData;
     }
 
+    /**
+     * show ori live page
+     * @throws CException
+     */
     public function actionLiveTest()
     {
         $toilets = Yii::app()->db->createCommand()
--- app/protected/views/toilet/charts.php
+++ app/protected/views/toilet/charts.php
@@ -29,14 +29,17 @@
 }
 
 .left-2 {
-    width: 70%;
-    margin: 5% auto;
+    width: 90%;
+    margin: 0 auto;
 }
 
 @media screen and (max-width: 736px) {
 	.all {
     	height: 400px;
     }
+	.actions {
+		display: none;
+	}
 }
 
 @media screen and (max-width: 620px) {
@@ -44,41 +47,95 @@
     	height: 250px;
     }
 }
+
+.date {
+	border: 1px solid #000000;
+	color: black;
+	margin: 0;
+}
+
 </style>
 <!-- Main -->
   <article id="main"> 
         <div class="all">
             <div class="left">
               <h3>廁所使用頻度分析圖表</h3>
+				<div style="width:100%;">
+					<ul class="actions" style="width:1285px; margin: 15px auto;">
+						<?php
+						$date = date('Y/m/d');
+						$startDate = date('Y/m/d', strtotime('-5 day'));
+						$endDate = date('Y/m/d');
+						for($i=$startDate ; $i<=$endDate ; $i=date('Y/m/d', strtotime('+1 day', strtotime($i)))){
+							$iForUrl = date('Y-m-d', strtotime($i));
+						?>
+						<li>
+							<a class="date button <?php if($i == $date) echo ' special'; ?>" href="javascript:;" onclick="getChart('<?php echo $iForUrl; ?>',$(this));">
+								<?php echo $i; ?>
+							</a>
+						</li>
+						<?php } ?>
+					</ul>
+				</div>
               <div class="left-2">
-				<canvas id="canvas"></canvas>
+				<canvas id="canvas" width="1200" height="450"></canvas>
               </div>
             </div>
         </div>
   </article>
 <script>
-	var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-	var lineChartData = {
-		//labels : ["January","February","March","April","May","June","July"],
-		labels : ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"],
-		datasets : [
-			{
-				label: "My Second dataset",
-				fillColor : "rgba(151,187,205,0.2)",
-				strokeColor : "rgba(151,187,205,1)",
-				pointColor : "rgba(151,187,205,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(151,187,205,1)",
-				data : <?php echo $datas; ?>
-			}
-		]
-	}
-
+	var lineChartData = getLineChartData(<?php echo $timeJson; ?>, <?php echo $datas; ?>)
+	var ctx;
+	var myLine;
 	window.onload = function(){
-		var ctx = document.getElementById("canvas").getContext("2d");
-		window.myLine = new Chart(ctx).Line(lineChartData, {
+		ctx = document.getElementById("canvas").getContext("2d");
+		myLine = new Chart(ctx).Line(lineChartData, {
 			responsive: true
 		});
+	}
+
+	function getChart(date, obj)
+	{
+		if(obj.hasClass('special')) return false;
+		$('.button').removeClass('special');
+		obj.addClass('special');
+
+		$.ajax({
+			url: '<?php echo $this->createUrl('toilet/getChart'); ?>',
+			data: 'date='+date,
+			type:"POST",
+			dataType:'json',
+			success: function(msg){
+				myLine.destroy();
+				lineChartData = getLineChartData(msg.timeJson, msg.datas);
+				myLine = new Chart(ctx).Line(lineChartData, {
+					responsive: true
+				});
+			},
+			error:function(xhr, ajaxOptions, thrownError){
+				alert(xhr.status);
+				alert(thrownError);
+			}
+		});
+	}
+
+	function getLineChartData(timeJson, datas)
+	{
+		var lineChartData = {
+			labels : timeJson,
+			datasets : [
+				{
+					label: "My Second dataset",
+					fillColor : "rgba(151,187,205,0.2)",
+					strokeColor : "rgba(151,187,205,1)",
+					pointColor : "rgba(151,187,205,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(151,187,205,1)",
+					data : datas
+				},
+			]
+		}
+		return lineChartData
 	}
 </script>
