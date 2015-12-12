af6f96df9bd13311a1b290a4e94959dd11b23eef
sq435 <sq435@hotmail.com>
2015-12-12 11:15:56 +0800

    調整default controller

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'HOLY SHIT',
-	'defaultController' => 'toilet/introduction',
+	'defaultController' => 'toilet/story',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
